10151e5ecbecc893f447f5c41e4b375f421556fa
Kush Singh Chibber <kushsinghraghvinder.chibber@student.griffith,ie>
2020-12-29 16:42:28 +0000

    CleanUp

--- whatsapp-client/src/components/Login.js
+++ whatsapp-client/src/components/Login.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Container, Form, Button } from "react-bootstrap";
-import { v4 as uuidv4 } from "uuid"
 import "./Login.scss";
 
 function Login({ onIdSubmit }) {
